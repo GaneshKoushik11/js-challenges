bf1e7fcd0ee3add62d2e74d0bd084651fcf64e48
GaneshKoushikBeroe <koushik.ganesh@beroe-inc.com>
2026-09-15 16:43:36 +0530

    feat: add secondLargest function to find second largest number in array

---
+++ secondLargest.js
@@ -0,0 +1,22 @@
+function secondLargest(numbers) {
+    let largest = -Infinity
+    let second = - Infinity
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > largest) {
+            second = largest
+            largest = numbers[i]
+        } else if (numbers[i] > second && numbers[i] != largest) {
+            second = numbers[i]
+        }
+    }
+    return second === -Infinity ? null : second;
+}
+
+console.log(secondLargest([10, 5, 8, 10, 3])); // 8
+console.log(secondLargest([5, 5, 4]));         // 4
+console.log(secondLargest([1, 2, 3]));         // 2
+console.log(secondLargest([-5, -2, -10]));     // -5
+console.log(secondLargest([7, 7, 7]));         // null
+console.log(secondLargest([5]));               // null
+console.log(secondLargest([]));                // null
