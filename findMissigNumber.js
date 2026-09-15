e98a19f4052386f43605f91a1d6ea2867f80a36f
GaneshKoushikBeroe <koushik.ganesh@beroe-inc.com>
2026-09-15 16:59:41 +0530

    feat: add findMissingNumber function to find missing values in an array

---
+++ findMissigNumber.js
@@ -0,0 +1,14 @@
+function findMissingNumber(numbers, n) {
+    let expectedSum = n * (n + 1) / 2
+    let actualSum = 0
+
+    for (let number of numbers) {
+        actualSum += number
+    }
+    return expectedSum - actualSum
+}
+
+console.log(findMissingNumber([1, 2, 4, 5], 5)); // 3
+console.log(findMissingNumber([2, 3, 4, 5], 5)); // 1
+console.log(findMissingNumber([1, 2, 3, 4], 5)); // 5
+console.log(findMissingNumber([], 1));           // 1
